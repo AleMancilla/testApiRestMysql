46076423b9bb6361c2057c7b38b2a067b8d3368a
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2025-02-06 19:15:45 -0400

    get

--- src/app.js
+++ src/app.js
@@ -15,16 +15,10 @@ app.use(express.json());
 // Routes
 app.use("/", indexRoutes);
 app.use("/api", databaseRoutes);
-
-app.post("/fbDelete", (req, res) => {
+app.get("/fbDelete", (req, res) => {
   const phpFilePath = path.join(__dirname, "fbdelete.php");
 
-  // Convertir los datos a formato de query string
-  const formData = Object.entries(req.body)
-      .map(([key, value]) => `${key}=${encodeURIComponent(value)}`)
-      .join("&");
-
-  exec(`php ${phpFilePath} ${formData}`, (error, stdout, stderr) => {
+  exec(`php ${phpFilePath}`, (error, stdout, stderr) => {
       if (error) {
           console.error(`Error ejecutando PHP: ${error.message}`);
           res.status(500).send("Error ejecutando PHP");
