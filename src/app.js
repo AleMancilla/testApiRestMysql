2e72604a095e37dd3b63e053bd7d6597b60d560f
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2025-02-06 02:13:24 -0400

    fbdelete

--- src/app.js
+++ src/app.js
@@ -15,6 +15,23 @@ app.use(express.json());
 // Routes
 app.use("/", indexRoutes);
 app.use("/api", databaseRoutes);
+app.get("/fbDelete", (req, res) => {
+  const phpFilePath = path.join(__dirname, "fbdelete.php");
+
+  exec(`php ${phpFilePath}`, (error, stdout, stderr) => {
+      if (error) {
+          console.error(`Error ejecutando PHP: ${error.message}`);
+          res.status(500).send("Error ejecutando PHP");
+          return;
+      }
+      if (stderr) {
+          console.error(`Error en PHP: ${stderr}`);
+          res.status(500).send(stderr);
+          return;
+      }
+      res.send(stdout);
+  });
+});
 
 app.use((req, res, next) => {
   res.status(404).json({ message: "Not found" });
