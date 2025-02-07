cb5d43556dfba93f3725570fb4d360f7516d3c50
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2025-02-07 00:27:45 -0400

    test

--- src/fbdelete.php
+++ src/fbdelete.php
@@ -1,41 +1,13 @@
 <?php
 header('Content-Type: application/json');
 
-$signed_request = $_POST['signed_request'];
-$data = parse_signed_request($signed_request);
-$user_id = $data['user_id'];
+$url = "https://example.com";
+$confirmation_code = "abc123";
 
-// Start data deletion
+$response = [
+    "url" => $url,
+    "confirmation_code" => $confirmation_code
+];
 
-$status_url = 'https://www.caferio.com/deletion?id=abc123'; // URL to track the deletion
-$confirmation_code = 'abc123'; // unique code for the deletion request
-
-$data = array(
-  'url' => $status_url,
-  'confirmation_code' => $confirmation_code
-);
-echo json_encode($data);
-
-function parse_signed_request($signed_request) {
-  list($encoded_sig, $payload) = explode('.', $signed_request, 2);
-
-  $secret = "e8d727a96f00fb94d751e9433ca81ef1"; // Use your app secret here
-
-  // decode the data
-  $sig = base64_url_decode($encoded_sig);
-  $data = json_decode(base64_url_decode($payload), true);
-
-  // confirm the signature
-  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
-  if ($sig !== $expected_sig) {
-    error_log('Bad Signed JSON signature!');
-    return null;
-  }
-
-  return $data;
-}
-
-function base64_url_decode($input) {
-  return base64_decode(strtr($input, '-_', '+/'));
-}
+echo json_encode($response, JSON_UNESCAPED_SLASHES);
 ?>
